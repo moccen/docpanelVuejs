586e646b6807ab9e0a789ea884b7f6dacb9afa5d
moccen <353373440@qq.com>
2016-01-20 16:35:18 +0800

    component test success!

--- js/myscript/myVue.js
+++ js/myscript/myVue.js
@@ -4,29 +4,21 @@ define("myVue", ["vue", "jquery"], function(Vue, $) {
 	module.init = function(contentData) {
 		this._contentData = contentData;
 
-//		var DocThumbComponent = Vue.extend({
-//			template: '<img style="width:80px;height:65px" v-bind:src="getSrc(file.fileType,file.href)" v-on:click="contentClickHandler(file.id)"/>'
-//		});
-//
-//		Vue.component('thumb-component', DocThumbComponent);
-//
-//		new Vue({
-//			el: '#content'
-//		});
+		//		var DocThumbComponent = Vue.extend({
+		//			template: '<img style="width:80px;height:65px" v-bind:src="getSrc(file.fileType,file.href)" v-on:click="contentClickHandler(file.id)"/>'
+		//		});
+		//
+		//		Vue.component('thumb-component', DocThumbComponent);
+		//
+		//		new Vue({
+		//			el: '#content'
+		//		});
 
-		var vmContent = new Vue({
-			el: "#content",
-			data: {
-				files: contentData
+		Vue.component('thumb-item',{
+			template:'#thumb-template',
+			props:{
+				files:Array
 			},
-			computed: {
-				nailSrc: function(fileType, src) {
-					return this.getSrc(fileType, src);
-				}
-			},
-			//						case "img":
-			//							return "/XZQHWebServer/readFile?filePath=" + encodeURI(encodeURI(src)) + "*thumbnail";
-			//							break;
 			methods: {
 				getSrc: function(fileType, src) {
 					switch (fileType) {
@@ -52,10 +44,56 @@ define("myVue", ["vue", "jquery"], function(Vue, $) {
 					alert("the node ID is " + nodeId);
 				}
 			},
-			template: '<img style="width:80px;height:65px" v-bind:src="getSrc(file.fileType,file.href)" v-on:click="contentClickHandler(file.id)"/>'
-
 		});
-
+		
+		var ctnt = new Vue({
+			el:".panel-body",
+			data:{
+				files:this._contentData
+			}
+		});
+		
+//		var vmContent = new Vue({
+//			el: "#content",
+//			data: {
+//				files: contentData
+//			},
+//			computed: {
+//				nailSrc: function(fileType, src) {
+//					return this.getSrc(fileType, src);
+//				}
+//			},
+//			//						case "img":
+//			//							return "/XZQHWebServer/readFile?filePath=" + encodeURI(encodeURI(src)) + "*thumbnail";
+//			//							break;
+//			methods: {
+//				getSrc: function(fileType, src) {
+//					switch (fileType) {
+//						case "text":
+//						case "unknown":
+//							return "./img/file.png";
+//							break;
+//
+//						case "dwg":
+//							return "./img/cad.png";
+//							break;
+//						case "video":
+//							return "./img/video.png";
+//							break;
+//						case "pdf":
+//							return "./img/pdf.png";
+//							break;
+//						default:
+//							return "./img/folder.png";
+//					}
+//				},
+//				contentClickHandler: function(nodeId) {
+//					alert("the node ID is " + nodeId);
+//				}
+//			},
+//			template: '<img style="width:80px;height:65px" v-bind:src="getSrc(file.fileType,file.href)" v-on:click="contentClickHandler(file.id)"/>'
+//
+//		});
 
 		var vmAccordion = new Vue({
 			el: "#leftNavigation",
@@ -63,16 +101,14 @@ define("myVue", ["vue", "jquery"], function(Vue, $) {
 				files: contentData,
 				items: contentData.nodes
 			},
-			compiled: function() {
+			ready: function() {
 				var accordion = new Accordion($('#leftNavigation'), false);
 			}
 		});
+
 //		Vue.nextTick(function() {
 //			var accordion = new Accordion($('#leftNavigation'), false);
 //		});
-
-
-
 
 	}
 
